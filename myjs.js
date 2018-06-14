50984fa37143518e8d23d0c3c7eef5afb5ba9856
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-14 12:55:30 -0500

    the script is logging to the console now
    the math isnt working right however.

--- myjs.js
+++ myjs.js
@@ -1,51 +1,50 @@
-slowMath.add(6, 2); {
-    return new Promise((resolve) => {
+slowMath.add(6, 2)
+    .then((result) => {
         console.log('I am starting the problem chain now');
-        console.log(num);
-        resolve(slowMath);
-    }).then(() => {
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part two of the promise chain starts here');
-        slowMath.multiply(num, 2);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.multiply(result, 2);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part three starts here!');
-        slowMath.divide(slowMath, 4);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.divide(result, 4);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part four starts here');
-        slowMath.subtract(slowMath, 3);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.subtract(result, 3);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part five starts here');
-        slowMath.add(slowMath, 98);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.add(result, 98);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part six starts here');
-        slowMath.divide(slowMath, 2);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.divide(result, 2);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part seven');
-        slowMath.multiply(slowMath, 50);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.multiply(result, 50);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part eight');
-        slowMath.divide(slowMath, 40);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
+        slowMath.divide(result, 40);
+        console.log(result);
+        return result;
+    }).then((result) => {
         console.log('part nine');
-        slowMath.add(slowMath, 30);
-        console.log(slowMath);
-        return slowMath;
-    }).then(() => {
-        console.log(`the final answer is ${slowMath}`);
+        slowMath.add(result, 30);
+        console.log(result);
+        return result;
+    }).then((result) => {
+        console.log(`the final answer is ${result}`);
     }).catch(() => {
         console.log(`Uh oh. Math machine broke.`);
-    })
-};
+    });
