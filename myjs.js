12a042924e1fdd6f57f47dd2363562e41c10a021
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-14 10:28:07 -0500

    added promise line to line 2

--- myjs.js
+++ myjs.js
@@ -1,49 +1,54 @@
-slowMath.add(6,2); {
-    console.log('I am starting the problem chain now');
-    console.log(num);
-    return num; 
-}).then (() => {
-    console.log('part two of the promise chain starts here');
-    slowMath.multiply(num, 2);
-    console.log(multTwo);
-    return multTwo;
-});.then (() => {
+function mathMachine() {
+    return new Promise((resolve) => {
+        console.log('I am starting the problem chain now');
+        slowMath.add(6, 2);
+        console.log(num);
+        resolve(num);
+    });
+}.then(() => {
+        console.log('part two of the promise chain starts here');
+        slowMath.multiply(num, 2);
+        console.log(multTwo);
+        return multTwo;
+    });
+});.then(() => {
     console.log('part three starts here!');
     slowMath.divide(multTwo, 4);
     console.log(divideFour);
     return divideFour;
-});.then (() => {
+});.then(() => {
     console.log('part four starts here');
     slowMath.subtract(divideFour, 3);
     console.log(subThree);
     return subThree;
-});.then (() => {
+});.then(() => {
     console.log('part five starts here');
     slowMath.add(subThree, 98);
     console.log(plus98);
     return plus98;
-});.then (() => {
+});.then(() => {
     console.log('part six starts here');
     slowMath.divide(plus98, 2);
     console.log(div2);
     return div2;
-});.then (() => {
+});.then(() => {
     console.log('part seven');
     slowMath.multiply(div2, 50);
     console.log(times50);
     return times50;
-});.then (() => {
+});.then(() => {
     console.log('part eight');
     slowMath.divide(times50, 40);
     console.log(div40);
     return div40;
-});.then (() => {
+});.then(() => {
     console.log('part nine');
     slowMath.add(div40, 30);
     console.log(plus30);
     return plus30;
-});.then (() => {
+});.then(() => {
     console.log(`the final answer is ${plus30}`);
-});.catch(() => {
+});.catch (() => {
     console.log(`Uh oh. Math machine broke.`);
 });
+mathMachine();
